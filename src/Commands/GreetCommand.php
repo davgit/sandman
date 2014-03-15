bed347e480bb9df5f38825393176ee9b49829327
Jerry Ablan <jerryablan@dreamfactory.com>
2014-03-15 17:35:56 -0400

    Bootstrap file created

---
+++ src/Commands/GreetCommand.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * This file is part of DreamFactory Sandman(tm)
+ *
+ * Copyright (c) 2014 DreamFactory Software, Inc. <support@dreamfactory.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace DreamFactory\Sandman\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * GreetCommand
+ */
+class GreetCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName( 'demo:greet' )
+            ->setDescription( 'Greet someone' )
+            ->addArgument(
+                'name',
+                InputArgument::OPTIONAL,
+                'Who do you want to greet?'
+            )
+            ->addOption(
+                'yell',
+                null,
+                InputOption::VALUE_NONE,
+                'If set, the task will yell in uppercase letters'
+            );
+    }
+
+    /**
+     * Executes the current command.
+     *
+     * This method is not abstract because you can use this class
+     * as a concrete class. In this case, instead of defining the
+     * execute() method, you set the code to execute by passing
+     * a Closure to the setCode() method.
+     *
+     * @param InputInterface  $input  An InputInterface instance
+     * @param OutputInterface $output An OutputInterface instance
+     *
+     * @return null|integer null or 0 if everything went fine, or an error code
+     *
+     * @throws \LogicException When this abstract method is not implemented
+     * @see    setCode()
+     */
+    protected function execute( InputInterface $input, OutputInterface $output )
+    {
+        $_name = $input->getArgument( 'name' );
+        $_name = 'Hello ' . ( $_name ? : null );
+
+        if ( $input->getOption( 'yell' ) )
+        {
+            $_name = strtoupper( $_name );
+        }
+
+        $output->writeln( $_name );
+    }
+}
